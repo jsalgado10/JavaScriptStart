c0c644efc35a0c7fffd1a3c9807328e2d4df438b
jsalgadodcr <jsalgado@dcrpos.com>
2022-10-18 18:34:40 -0500

    Add delete link and function

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ console.log(isValid(null));
 let filteredData = data;
 
 const state = {
-  item: data,
+  items: data,
   currentItem: {
     name: "",
     size: "",
@@ -45,6 +45,26 @@ for (let input of inputs) {
   input.addEventListener("change", changeState);
 }
 
+const deleteItem = (id) => {
+  const itemIndex = state.items.findIndex((i) => i.id === id);
+  if (itemIndex >= 0) {
+    const tempData = Array.from(state.items);
+    tempData.splice(itemIndex, 1);
+    state.items = tempData;
+    filteredData = tempData;
+    buildTable();
+  }
+};
+
+const buildDeleteLinks = () => {
+  const deletes = document.querySelectorAll("td[data-delete]");
+  for (let del of deletes) {
+    del.addEventListener("click", (e) => {
+      deleteItem(+e.currentTarget.id.substring(3));
+    });
+  }
+};
+
 const buildTable = () => {
   console.log(filteredData);
   let html = '<table style="width:90%; margin: 20px auto; color: #000">';
@@ -59,11 +79,13 @@ const buildTable = () => {
     html += `<td>${size}</td>`;
     html += `<td>${price}</td>`;
     html += `<td>${category}</td>`;
-    html += `<td><button>Delete</button></td>`;
+    html += `<td style="cursor:pointer" id="tr-${id}" data-delete=${id}></td>`;
     html += `</tr>`;
   });
   html += `</table>`;
   document.getElementById("items").innerHTML = html;
+
+  buildDeleteLinks();
 };
 
 buildTable();
@@ -79,11 +101,37 @@ Array.prototype.unique = function (field) {
   return newArray;
 };
 
-const filterData = (property) => {
-  return function (value) {
-    return data.filter((i) => i[property] === value);
-  };
+// const filterData = (property) => {
+//   return function (value) {
+//     return data.filter((i) => i[property] == value);
+//   };
+// };
+
+const onFilterChange = (element) => {
+  if (element.target.value === "0") {
+    filteredData = data;
+  } else {
+    filteredData = state.items.filter(
+      (d) => d.category === element.target.value
+    );
+  }
+  buildTable();
 };
+
+const buildFilterBox = () => {
+  const categories = data.unique("category");
+  let html = '<select id="category-filter">';
+  html += '<option value="0">Select Category </option>';
+  categories.map((category) => {
+    html += `<option value="${category}">${category}</option>`;
+  });
+  html += "</select>";
+  document.getElementById("filter").innerHTML = html;
+  const newSelect = document.getElementById("category-filter");
+  newSelect.addEventListener("change", onFilterChange);
+};
+
+buildFilterBox();
 
 // const curriedFilter = filterData("category");
 // const fruits = curriedFilter("fruit");
