38843c8d2983cab51bf5890528d110fa70d2dbe8
jsalgadodcr <jsalgado@dcrpos.com>
2022-10-18 17:53:02 -0500

    Add Table and styles

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Javascript Start
+
+[![CircleCI](https://dl.circleci.com/status-badge/img/gh/jsalgado10/JavaScriptStart/tree/master.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/jsalgado10/JavaScriptStart/tree/master)
+![GitHub](https://img.shields.io/github/license/jsalgado10/JavaScriptStart)
+
+[Shields Link](https://img.shields.io/github/license/jsalgado10/JavaScriptStart) - link for badges
+
+[License site](https://choosealicense.com/licenses/mit) - Site for License Badge
+
+## Branch 6
+
+We Added our table and styles
--- src/index.js
+++ src/index.js
@@ -16,3 +16,80 @@ const state = {
     category: "",
   },
 };
+
+const changeState = (element) => {
+  const { id, value } = element.target;
+  if (!isValid(id) || !isValid(value)) return;
+
+  setValue(id, value);
+
+  const result = {
+    ...state,
+    currentItem: {
+      ...(state.currentItem[id] = value),
+    },
+  };
+  console.log(result);
+  return result;
+};
+
+const setValue = (id, value) => {
+  if (isValid(value)) {
+    document.getElementById(id).value = value;
+  }
+};
+
+const inputs = document.getElementsByTagName("input");
+
+for (let input of inputs) {
+  input.addEventListener("change", changeState);
+}
+
+const buildTable = () => {
+  console.log(filteredData);
+  let html = '<table style="width:90%; margin: 20px auto; color: #000">';
+  html += "<tr><th>Product</th>";
+  html += "<th>Size</th>";
+  html += "<th>Price</th>";
+  html += "<th>Category</th>";
+  html += "<th>Delete</th></tr>";
+  filteredData.map((item) => {
+    const { name, id, price, category, size } = item;
+    html += `<tr><td>${name}</td>`;
+    html += `<td>${size}</td>`;
+    html += `<td>${price}</td>`;
+    html += `<td>${category}</td>`;
+    html += `<td><button>Delete</button></td>`;
+    html += `</tr>`;
+  });
+  html += `</table>`;
+  document.getElementById("items").innerHTML = html;
+};
+
+buildTable();
+
+Array.prototype.unique = function (field) {
+  const newArray = [];
+  this.forEach((record) => {
+    const { [field]: targetField } = record;
+    if (!newArray.includes(targetField)) {
+      newArray.push(targetField);
+    }
+  });
+  return newArray;
+};
+
+const filterData = (property) => {
+  return function (value) {
+    return data.filter((i) => i[property] === value);
+  };
+};
+
+// const curriedFilter = filterData("category");
+// const fruits = curriedFilter("fruit");
+// const drinks = curriedFilter("beverages");
+// const candy = curriedFilter("candy");
+
+// console.log(fruits);
+// console.log(drinks);
+// console.log(candy);
